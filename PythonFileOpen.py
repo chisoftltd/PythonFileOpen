eadbfb088bf1f1ee6d68d061729a8394f9db4aea
chisoftltd <chisoftltd@gmail.com>
2021-02-25 16:05:01 +0000

    Read Only Parts of the File

--- PythonFileOpen.py
+++ PythonFileOpen.py
@@ -1,3 +1,12 @@
 # Python File Open
 f = open("demofile.txt", "a")
-f.write("My Training in Python")
+f.write("\nMy Training in Python")
+
+# Open a File on the Server
+f = open("demofile.txt", "r")
+print(f.read())
+print()
+
+# Read Only Parts of the File
+f = open("demofile.txt", "r")
+print(f.read(13))
